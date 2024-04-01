e2f411147d62e7b038fa636fcb1caf3ca84398dd
kuleshov-aleksei <alexxey67@gmail.com>
2024-04-01 22:33:17 +0300

    Allow configure execute as root

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ RUN apt update && apt install -y build-essential clang flex bison g++ gawk \
 gcc-multilib g++-multilib gettext git libncurses5-dev libssl-dev \
 python3-distutils rsync unzip zlib1g-dev file wget subversion git-core 
 
+ENV FORCE_UNSAFE_CONFIGURE=1
+
 WORKDIR /build
 
 CMD ["tail", "-f", "/dev/null"]
--- deps/build.sh
+++ deps/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 OPENWRT_BRANCH=openwrt-23.05
 
 mkdir -p /vendor/
 cd /vendor/
-git clone -b $(OPENWRT_BRANCH) https://github.com/kuleshov-aleksei/openwrt-bpi-r4 openwrt
+git clone -b $OPENWRT_BRANCH https://github.com/kuleshov-aleksei/openwrt-bpi-r4 openwrt
 
 cd /vendor/openwrt
 cp /deps/.config /vendor/openwrt/.config
@@ -12,7 +14,7 @@ cp /deps/.config /vendor/openwrt/.config
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-# Run this to customize build
+# Run this manually to customize build
 #make menuconfig
 
 make -j$(nproc) defconfig download clean world
