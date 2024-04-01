4a6d40d51abe2a8f1c17bb794bb01599e2ad451c
kuleshov-aleksei <alexxey67@gmail.com>
2024-04-01 22:27:22 +0300

    Prepare config for openwrt

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ docker build -t temp_openwrt_build .
 ```
 docker run --rm `
   --name openwrt_build `
+  -v ${PWD}/deps:/deps `
   -v ${PWD}/build:/build `
   temp_openwrt_build
 ```
--- build.sh
+++ build.sh
@@ -7,10 +7,12 @@ cd /vendor/
 git clone -b $(OPENWRT_BRANCH) https://github.com/kuleshov-aleksei/openwrt-bpi-r4 openwrt
 
 cd /vendor/openwrt
+cp /deps/.config /vendor/openwrt/.config
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-make menuconfig
+# Run this to customize build
+#make menuconfig
 
 make -j$(nproc) defconfig download clean world
